c749665f6863f7ee096800af1b8357b3da356cd1
Kostya <koooooostaaa@gmail.com>
2023-04-18 20:39:39 +0300

    install fontawesome

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
 
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {faCartShopping, faXmark} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+
+library.add(faCartShopping, faXmark)
+
 const app = createApp(App)
 
 app.use(router)
-
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
