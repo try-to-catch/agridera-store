fb26302aa9a30c89cb8aac9879c91b463e851c86
Kostya <koooooostaaa@gmail.com>
2023-04-18 20:42:40 +0300

    Add the ability to add a product to the cart

--- src/views/products/ShowProducts.vue
+++ src/views/products/ShowProducts.vue
@@ -1,6 +1,6 @@
 <template>
     <default-header/>
-    <main v-if="isProductSet" class="pb-28 md:pb-[168px]">
+    <main v-if="product" class="pb-28 md:pb-[168px]">
         <section class="text-custom-green my-[18px] mx-2.5 lg:container lg:mx-auto">
             <router-link :to="{ name: 'products.index'}" class="flex ">
                 <img alt="arrow" class="w-6" src="@/assets/images/icons/arrow.svg">
@@ -8,12 +8,32 @@
             </router-link>
         </section>
         <section
-                class="lg:container lg:mx-auto mx-2.5 grid grid-cols-1 lg:grid-cols-2 md:gap-x-11 justify-items-center lg:justify-items-start">
+                class="lg:container lg:mx-auto mx-2.5 flex flex-col-reverse lg:flex-row md:gap-x-11 justify-items-center lg:justify-items-start">
+            <div class="flex flex-col items-center">
+                <div class="max-h-[500px] max-w-[540px] overflow-hidden">
+                    <img :src="product.image" alt="product_image">
+                </div>
+                <div class="mt-[30px] max-w-[540px] w-full lg:max-w-full lg:w-full">
+                    <button v-if="isAddButtonActive"
+                            class="text-custom-green w-full space-x-5 py-2.5 justify-center px-[30px] border-custom-green border-2 flex items-center rounded-[60px]"
+                            @click="addProductToCart(product.id)">
+                        <font-awesome-icon :icon="['fas', 'cart-shopping']"/>
+                        <span class="font-bold text-[17px]">Add to cart</span>
+                    </button>
+                    <router-link v-else :to="{name: 'contactUs'}"
+                                 class="text-custom-green w-full space-x-5 py-2.5 justify-center px-[30px] border-custom-green border-2 flex items-center rounded-[60px]"
+
+                    >
+                        <font-awesome-icon :icon="['fas', 'cart-shopping']"/>
+                        <span class="font-bold text-[17px]">Go to cart</span>
+                    </router-link>
+                </div>
+            </div>
             <div>
                 <div class="lg:text-start text-center">
                     <h1 class="font-bold text-custom-green text-4xl  lg:text-5xl">
                         <span class="text-custom-gray-900">{{ product.title }}</span>
-                        {{ product?.short_text }}
+                        {{ product.short_text }}
                     </h1>
                     <div class="pt-10 lg:block hidden text-custom-green text-[20px]">*****</div>
                 </div>
@@ -27,18 +47,7 @@
                     </h4>
                 </div>
             </div>
-            <div class="flex flex-col">
-                <div class="max-h-[500px] max-w-[540px] overflow-hidden">
-                    <img alt="product_image" :src="product.image">
-                </div>
-                <div class="self-end mt-[30px] w-full lg:w-fit">
-                    <a class="text-custom-green space-x-5 py-2.5 justify-center px-[30px] border-custom-green border-2 flex items-center rounded-[60px]"
-                       href="#">
-                        <img alt="ico-pdf" src="@/assets/images/icons/ico-pdf.svg">
-                        <span class="font-bold text-[17px]">Download PDF</span>
-                    </a>
-                </div>
-            </div>
+
         </section>
         <section class="lg:container lg:mx-auto mx-2.5 pt-10">
             <h5 class="text-3xl font-bold">
@@ -49,19 +58,30 @@
     </main>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
 import DefaultHeader from "@/components/DefaultHeader.vue";
 import useProducts from "@/composables/products";
-import {computed, onMounted, ref} from "vue";
+import type {Ref} from "vue"
+import {onMounted, ref} from "vue";
+import type {IProduct} from "@/types/IProduct";
+import useCart from "@/composables/cart";
 
 const props = defineProps<{ routeId: number }>()
 
 const {getProduct} = useProducts()
+const {addToCart, isProductInCart} = useCart()
 
-const product = ref()
+const product: Ref<IProduct | undefined> = ref()
+const isAddButtonActive = ref(true)
+
 onMounted(() => {
     product.value = getProduct(props.routeId)
+    isAddButtonActive.value = !isProductInCart(props.routeId)
 })
 
-const isProductSet = computed(() => product.value)
+
+const addProductToCart = (id: number) => {
+    addToCart(id)
+    isAddButtonActive.value = false
+}
 </script>
